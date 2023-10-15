ecf76dcb6785460a6edb41147dc3ae7055267e84
segomezz <141938389+segomezz@users.noreply.github.com>
2023-10-14 19:13:07 -0500

    Update Futbolista.py

--- Futbolista.py
+++ Futbolista.py
@@ -8,18 +8,18 @@ class Futbolista (Deportista):
         self._piernaHabil=piernaHabil
         Futbolista.listaFutbolista.append(self)
 
-        def getGolesMarcados(self):
-            return self._golesMarcados
-        def setGolesMarcados(self,golesMarcados):
-            self._golesMarcados=golesMarcados
-        def getTarjetasRojas(self):
-            return self._tarjetasRojas
-        def setTarjetasRojas(self,tarjetasRojas):
-            self._tarjetasRojas=tarjetasRojas
-        def getPiernaHabil(self):
-            return self._piernaHabil
-        def setPiernaHabil(self,piernaHabil):
-            self._piernaHabil=piernaHabil
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self,golesMarcados):
+        self._golesMarcados=golesMarcados
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self,tarjetasRojas):
+        self._tarjetasRojas=tarjetasRojas
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def setPiernaHabil(self,piernaHabil):
+        self._piernaHabil=piernaHabil
     def __str__(self):
         cadena = "Mi nombre es {} soy profesional en el deporte {} Tengo {} años de edad y llevo {} años en el deporte".format(self.getNombre(), self.getDeporte(), str(self.getEdad()), str(self.getAñosPracticando()))
         return cadena
